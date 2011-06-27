9a344e75689b16328c619a617363af12838fd635
Tom de Bruijn <tom@newanz.com>
2011-06-27 17:51:11 +0200

    Gitignore more stuff and auto filename generation

--- screenshot.rb
+++ screenshot.rb
@@ -1,20 +1,21 @@
 class Screenshot
   include Java
 
-  import java.awt.Desktop # added Desktop to import
+  import java.awt.Desktop
   import java.awt.Robot
   import java.awt.Toolkit
   import java.awt.Rectangle
   import javax.imageio.ImageIO
 
-  def self.capture(filename = 'screenshot.png')
+  def self.capture()
     robot     = Robot.new
     toolkit   = Toolkit.get_default_toolkit
     dim       = toolkit.get_screen_size
     rectangle = Rectangle.new(0, 0, dim.get_width, dim.get_height)
     image     = robot.create_screen_capture(rectangle)
-
-    file  = java::io::File.new(filename)
+    
+    time_now = Time.now
+    file  = java::io::File.new("screenshot_#{time_now.strftime('%Y_%m_%d_%H_%M_%S')}.png")
     ImageIO::write(image, "png", file)
 
     # Open the file in the users default application for the given file type
