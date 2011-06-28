0e55c3aaf4d2536feec3b5580ac80439a838c7d5
Tom de Bruijn <tom@newanz.com>
2011-06-29 01:14:12 +0200

    Cleanup for SelectionLayer class.

--- bin/jar-bootstrap.rb
+++ bin/jar-bootstrap.rb
@@ -1,18 +1,20 @@
 require 'tray_application'
-# require 'screenshot'
 require 'selection_layer'
 import com.sun.awt.AWTUtilities
 
-app = TrayApplication.new("Deskshot")
+app = TrayApplication.new("Screenshot grabber")
 app.icon_filename = '../icon.gif'
+
 app.item('Take Screenshot') do
-  frame = SelectionLayer.start
+  frame = SelectionLayer.new
   begin
     AWTUtilities.setWindowOpacity(frame, (0.8).to_f)
   rescue
     puts "ERROR: Transparant layer not supported. Running GNOME?"
   end
 end
+
 app.item('Exit') { java.lang.System::exit(0) }
 app.run
+
 puts "Started tray"
--- bin/selection_layer.rb
+++ bin/selection_layer.rb
@@ -2,61 +2,57 @@ include Java
 
 require 'screenshot'
 
-import java.awt.Desktop
+import java.awt.Color
 import java.awt.Robot
 import java.awt.Toolkit
-import java.awt.Rectangle
-import javax.imageio.ImageIO
-import javax.swing.JFrame
-import javax.swing.JLabel
-import javax.swing.JComponent
 import java.awt.MouseInfo
 import java.awt.PointerInfo
+import javax.swing.JFrame
+import javax.swing.JComponent
+import java.awt.event.MouseAdapter
 
 class SelectionLayer < JFrame
 
-  def self.start()
+  def initialize
+    super('Transparant layer')
+    
+    # Get the size of the screen
     robot     = Robot.new
     toolkit   = Toolkit.get_default_toolkit
     dim       = toolkit.get_screen_size
-    # rectangle = Rectangle.new(0, 0, dim.get_width, dim.get_height)
-    # image     = robot.create_screen_capture(rectangle)
     
-    @frame = SelectionLayer.new 'Transparant layer'
-    @frame.set_bounds 0,0,dim.get_width, dim.get_height
-    # @frame.default_close_operation = JFrame::EXIT_ON_CLOSE
+    # Create a fullscreen frame
+    self.set_bounds(0,0,dim.get_width, dim.get_height)
+    # No borders
+    self.set_undecorated(true)
+    self.visible = true
     
-    @frame.set_undecorated true
-    @frame.visible = true
-    
-    # get_content_pane().add JLabel.new("#{MouseInfo.getPointerInfo().getLocation()}")
-    @frame.cg()
-    @frame
+    # Add a draw panel to make something selectable
+    can = SelectionCanvas.new(self)
+      # MouseAction records mouse movements
+      action = MouseAction.new(can)
+      can.addMouseListener(action)
+      can.addMouseMotionListener(action)
+    getContentPane.add(can)
+
+    self # Needed to make the frame transparent later
   end
-  
-  def cg
-    can = SelectionCanvas.new self
-    action = MouseAction.new(can)
-    can.addMouseListener action
-    can.addMouseMotionListener action
-    getContentPane().add(can)
-  end
+
 end
 
-import java.awt.event.MouseAdapter
-import java.awt.Color
 class SelectionCanvas < JComponent
   include java.awt.event
   attr_accessor :frame
   
   def initialize(f)
-    super()
+    super
     self.frame = f
   end
   
   def draw_rect(x,y,x2,y2)
-    g = get_graphics()
+    g = get_graphics
     g.setColor(Color.red)
+    # Left to right or right to left drag?
     if x < x2
       g.drawRect(x, y, x2-x, y2-y)
     else
@@ -74,23 +70,31 @@ class MouseAction < MouseAdapter
   attr_accessor :canvas, :startpoint
   
   def initialize(c)
-    super()
+    super
     self.canvas = c
   end
 
   def mousePressed(e)
+    # Save startpoint, needed for creating a screenshot
     self.startpoint = e.get_point
   end
   
   def mouseDragged(e)
+    # Draw selection
     self.canvas.draw_rect(self.startpoint.x,self.startpoint.y,e.get_point().x,e.get_point().y)
   end
 
   def mouseReleased(e)
+    # Draw final selection
     self.canvas.draw_rect(self.startpoint.x,self.startpoint.y,e.get_point().x,e.get_point().y)
+    # Exit layer
     self.canvas.close()
+    
+    # Default size, just in case something fails
     width = 200
     height = 200
+    
+    # Left to right or right to left drag?
     if self.startpoint.x < e.get_point().x
       width = e.get_point().x - self.startpoint.x
       height = e.get_point().y - self.startpoint.y
@@ -98,6 +102,7 @@ class MouseAction < MouseAdapter
       width = self.startpoint.x - e.get_point().x
       height = self.startpoint.y - e.get_point().y
     end
+    
     # Create screenshot but cut off the red outer rectangle
     width -= 1
     height -= 1
