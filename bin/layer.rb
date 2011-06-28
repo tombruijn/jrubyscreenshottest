d71fffa8127d61eff4023e070c44f614f28ed625
Tom de Bruijn <tom@newanz.com>
2011-06-28 23:21:42 +0200

    More drawing

--- bin/layer.rb
+++ bin/layer.rb
@@ -22,55 +22,69 @@ class Layer
     
     @frame = JFrame.new 'Transparant layer'
     @frame.set_bounds 0,0,dim.get_width, dim.get_height
-    @frame.default_close_operation = JFrame::EXIT_ON_CLOSE
+    # @frame.default_close_operation = JFrame::EXIT_ON_CLOSE
     
     @frame.set_undecorated true
     @frame.visible = true
     
     @frame.get_content_pane().add JLabel.new("#{MouseInfo.getPointerInfo().getLocation()}")
-    can = MyCanvas.new
-    can.addMouseListener MouseAction.new(can)
+    @frame.create_graphics
     @frame.getContentPane().add can
     @frame
+  end
+  
+  def create_graphics
+    can = MyCanvas.new @frame
+    can.addMouseListener MouseAction.new(can)
+    can.addMouseMotionListener MouseAction.new(can)
   end
 end
 
 import java.awt.event.MouseAdapter
+import java.awt.Color
 class MyCanvas < JComponent
   include java.awt.event
   
-  def initialize
+  @frame
+  def initialize(frame)
+    @frame = frame
     # addMouseListener(this)
   end
   
-  def paint(g)
-    # g.drawRect(10, 10, 200, 200)
-    
-  end
-  
   def draw_rect(x,y,x2,y2)
+    # repaint()
     g = get_graphics()
+    paint(g)
+    g.setColor(Color.red)
     # g.dispose()
-    # g.paint()
-    g.drawRect(x, y, x2-x, y2-y)
+    if x < x2
+      g.drawRect(x, y, x2-x, y2-y)
+    else
+      g.drawRect(x2, y2, x-x2, y-y2)
+    end
   end
   
 end
 
 class MouseAction < MouseAdapter
   @fr = nil
-  @point = nil
+  @@startpoint = nil
   def initialize(frame)
     super()
     @fr = frame
   end
 
   def mousePressed(e)
-    @point = e.get_point
+    @@startpoint = e.get_point
+  end
+  
+  def mouseDragged(e)
+    # puts "wa"
+    @fr.draw_rect(@@startpoint.x,@@startpoint.y,e.get_point().x,e.get_point().y)
   end
 
-  def mouseReleased e
-    @fr.draw_rect(@point.x,@point.y,e.get_point().x,e.get_point().y)
+  def mouseReleased(e)
+    @fr.draw_rect(@@startpoint.x,@@startpoint.y,e.get_point().x,e.get_point().y)
   end
   
 end
